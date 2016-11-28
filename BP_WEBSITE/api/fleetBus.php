dbe0a04a58590948e015f4b7a3e728737ee8b1b3
Redevil <stefano.badalucco@mail.polimi.it>
2016-11-28 10:34:33 +0100

    .
    .

--- BP_WEBSITE/api/fleetBus.php
+++ BP_WEBSITE/api/fleetBus.php
@@ -2,14 +2,27 @@
 header("Access-Control-Allow-Origin: *");
 ?>
 
+<div class = "intestation">
+    <div class = "row" >
+        <div class="col-md-6 col-sm-6 col-xs-6" >
+            <h3 id = "titleIntestation" >Bus Management</h3>
+            <h4 id = "minimalDescription">
+                Here you can add, remove, or modify our buses
+            </h4>
+        </div>
+        <div class="col-md-6 col-sm-6 col-xs-6">
+            <img src = "../Images/modifyBuses.jpg" class = "intestationImages"  >
+        </div>
+    </div>
+</div>
+
 <div>
     <div class="row">
+        <!--Bus list from database, button to add a bus -->
         <div class="col-md-6 col-sm-6 col-xs-6">
-           <img src="Images/modifyBusses.jpg">
         </div>
-        <div class="col-md-6 col-sm-8 col-xs-6">
-            <h6>Modify Buses</h6>
-            <h4>Here you can </h4>
+        <!-- Map with the location of all the bus -->
+        <div class="col-md-6 col-sm-6 col-xs-6">
         </div>
     </div>
 </div>
