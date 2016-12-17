506ccc21be1cc74abcf9a0375d694902346af5b6
HuyHoangNguyen <huyhoang211291@gmail.com>
2016-12-17 21:46:28 +0100

    Commit timetable

--- Python code/src2/test.py
+++ Python code/src2/test.py
@@ -6,7 +6,6 @@ import json
 from src2.request_simulator.request_simulator import *
 from firebase import firebase
 
-
 RequestSimulator = UserRequestSimulator()
 RequestSimulator.clear_user_request_table()
 RequestSimulator.generate_user_request(int(sys.argv[1]),int(sys.argv[2]))
